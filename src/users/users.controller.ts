b9374cf2e7027e2b8e01d39f297d2cf1b40bfcdd
Dylan R <90897072+Aestorgs@users.noreply.github.com>
2022-12-19 14:58:46 +0100

    Update users.controller.ts

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -5,15 +5,19 @@ import { UsersService } from './users.service';
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
+  // je crée une route pour afficher tout les utilisateurs
   @Get('')
   getUsers() {
     return this.usersService.findAll();
   }
+  
+  // je crée une route pour afficher les utilisateur par id avec leurs matierre 
   @Get(':id')
   getUser(@Param('id', ParseIntPipe) id: number) {
     return this.usersService.findById(id);
   }
 
+  //je crée une route pour afficher les utilisateur par id avec leurs matierre avec leurs moyennes
   @Get(':id/matieres')
   getUsersMatiere(@Param('id', ParseIntPipe) id: number) {
     return this.usersService.findByMatiereId(id);
