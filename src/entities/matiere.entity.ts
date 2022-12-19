e3c12e6db4a04bf381cfc567d6a640a2b1451b07
Dylan R <90897072+Aestorgs@users.noreply.github.com>
2022-12-19 14:52:51 +0100

    Update matiere.entity.ts

--- src/entities/matiere.entity.ts
+++ src/entities/matiere.entity.ts
@@ -1,6 +1,7 @@
 import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
 import { User } from './users.entety';
 
+/ je crée la table et crée les colonne 
 @Entity('matieres')
 export class Matieres {
   @PrimaryGeneratedColumn()
